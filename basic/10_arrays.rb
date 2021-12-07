e1c70af84a43117aab83266b58f67741744a6b86
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 23:03:06 +0330

    add array section

---
+++ basic/10_arrays.rb
@@ -0,0 +1,27 @@
+
+# index             0        1        2
+friends = Array["Kevin", "Karen", "Oscar"]  
+
+# different type of variable
+array1 = Array[1, 2, 3, true, "Name", 20.3]
+
+# index
+puts friends[0]
+puts friends[-2]
+puts friends[100]
+
+
+# update an element
+friends[0] = "Dwight"
+
+puts friends[0]
+
+
+# an empty array
+new_friends = Array.new
+
+new_friends[0] = "Oscar"
+new_friends[5] = "Kevin"
+new_friends[15] = "Karen"
+
+puts new_friends
