71bc439f5062a9704a5f845866fe4859c3d0c2bd
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 22:41:12 +0330

    update math section

--- basic/06_math.rb
+++ basic/06_math.rb
@@ -12,3 +12,19 @@ puts 5
 puts 5 * 9
 puts 2 ** 3
 puts 10 % 3
+
+
+# methods on int
+puts -20.abs()
+puts 20.1.floor()
+puts 20.1.ceil()
+puts 20.1.round()
+puts Math.sqrt(36)
+puts Math.log(1)
+
+
+# float and int
+puts 1.1 + 10  # result is float
+puts 1.1 + 2.1
+puts 10 / 7  # result is int
+puts 10 / 7.0  # result is float
