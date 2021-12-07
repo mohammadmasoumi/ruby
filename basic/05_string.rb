542d25a9236b1b755bf859b30dac3860bea05ad6
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 22:33:36 +0330

    add string

--- basic/05_string.rb
+++ basic/05_string.rb
@@ -1,19 +1,25 @@
 
-
+# escape characters
 puts "Hello \" world"
-
 puts "Hello \n world"
 
-
+# variable
 phrase = "Hello world"
 
+# methods on string
 puts phrase
 puts phrase.upcase()
 puts phrase.downcase()
 puts phrase.strip()
 puts phrase.length()
 puts phrase.include? "Hello"
+puts "programming".upcase()
 
-
+# indexing
 puts phrase[100]  # no exception
 puts phrase[10]
+puts phrase[0, 5]  # range index
+puts phrase.index("Hell")
+puts phrase.index("H")
+puts phrase.index("M")  # no exception
+
