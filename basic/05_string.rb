b9b33a2818ded17fd004c7be92e87fd56ac02ec7
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 22:31:01 +0330

    add string

--- basic/05_string.rb
+++ basic/05_string.rb
@@ -5,9 +5,15 @@ puts "Hello \" world"
 puts "Hello \n world"
 
 
-phrase = "  Hello world  "
+phrase = "Hello world"
 
 puts phrase
 puts phrase.upcase()
 puts phrase.downcase()
 puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Hello"
+
+
+puts phrase[100]  # no exception
+puts phrase[10]
