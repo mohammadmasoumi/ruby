0b1e9b0b9e5983b0f9e185a82824e6c3190a4c87
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 23:19:55 +0330

    add method section

---
+++ basic/12_methods.rb
@@ -0,0 +1,23 @@
+
+puts "Enter your name: "
+name = gets.chomp()
+
+puts "Enter your age: "
+age = gets.chomp()
+
+
+#               default values
+def sayhi(name="Mohammad", age="25")
+    puts ("Hello " + name + ", you are " + age)
+end
+
+
+puts "Top"
+
+sayhi(name, age)
+sayhi(name)
+sayhi
+
+
+
+puts "Buttom"
