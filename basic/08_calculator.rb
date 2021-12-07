05462f6ba898dce084ca58b47836c29bee6a6488
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 22:50:47 +0330

    add calculator

---
+++ basic/08_calculator.rb
@@ -0,0 +1,12 @@
+puts "Enter a number: "
+num1 = gets.chomp()
+
+puts "Enter another number: "
+num2 = gets.chomp()
+
+# string concatenation
+puts (num1 + num2)
+
+# first cast them to the int, then add them
+puts (num1.to_i + num2.to_i)
+puts (num1.to_f + num2.to_f)
