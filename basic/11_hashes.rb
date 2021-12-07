5cbe1601f9bb182dff9c622289804dbb8e012d08
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 23:10:56 +0330

    add hashes

---
+++ basic/11_hashes.rb
@@ -0,0 +1,17 @@
+
+
+# hashes are like dictionaries
+# New York -> NT
+# california -> CA
+
+states = {
+    "California" => "CA",
+    "Pennsylvania" => "PA",
+    "Oregon" => "OR",
+    :Texas => "TS",
+    1 => "WS"
+}
+
+puts states["Oregon"]
+puts states[:Texas]
+puts states[1]
