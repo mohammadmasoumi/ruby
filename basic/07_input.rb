bfdcb0ffbfc5ce6a8ee8533def19c381da98a3ae
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-07 22:46:58 +0330

    add input section

---
+++ basic/07_input.rb
@@ -0,0 +1,13 @@
+# get input from user
+
+puts "Enter your name:"
+name = gets.chomp()  # grabs the new line too!
+
+puts "Enter your age:"
+age = gets.chomp()  # grabs the new line too!
+
+
+puts "Hello " + name + ", you are cool!" 
+puts "You are " + age
+
+
